714dbb5282081b627c515d66a57f98d4579e2932
韩雪滢 <hanxueying@hanxueyingdemacbook-pro.local>
2016-08-17 15:35:47 +0800

    Initial Commit

---
+++ Alumn/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Alumn
+//
+//  Created by 韩雪滢 on 8/17/16.
+//  Copyright © 2016 韩雪滢. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Alumn/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Alumn
+//
+//  Created by 韩雪滢 on 8/17/16.
+//  Copyright © 2016 韩雪滢. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
